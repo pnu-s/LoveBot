497aa5f8d5459c7a58b9d926075d3e9ace008d88
SimonPruneau <kartsim.pruneau@gmail.com>
2013-12-03 08:52:45 +0100

    First commit

---
+++ gen/com/example/lovebot/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.lovebot;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/lovebot/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.lovebot;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    requestWindowFeature(Window.FEATURE_LEFT_ICON);
+		setContentView(R.layout.activity_main);
+	    getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,R.drawable.ic_launcher);
+		View v = findViewById(android.R.id.title);
+		v.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				afficherAuthors(v);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+
+		setTitle("Test");
+		//getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
+		//R.layout.title);
+		return true;
+	}
+	
+	public void afficherAuthors(View v) {
+		new AlertDialog.Builder(this).setTitle("Auteurs")
+				.setMessage("S. Pruneau & H. Burlini").show();
+	}
+
+}
