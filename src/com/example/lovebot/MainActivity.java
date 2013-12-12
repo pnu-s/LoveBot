01e93d2b197c1788aae4b44129b2685f8c1f6ba4
SimonPruneau <kartsim.pruneau@gmail.com>
2013-12-12 16:15:29 +0100

    Ajout des autorisations nécessaires dans AndroidManifest. Liste des contacts avec nom et num sur la page d'accueil.

--- src/com/example/lovebot/MainActivity.java
+++ src/com/example/lovebot/MainActivity.java
@@ -1,27 +1,57 @@
 package com.example.lovebot;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.database.Cursor;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	    requestWindowFeature(Window.FEATURE_LEFT_ICON);
+		requestWindowFeature(Window.FEATURE_LEFT_ICON);
 		setContentView(R.layout.activity_main);
-	    getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,R.drawable.ic_launcher);
+		getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
+				R.drawable.ic_launcher);
 		View v = findViewById(android.R.id.title);
 		v.setOnClickListener(new OnClickListener() {
 			public void onClick(View v2) {
 				afficherAuthors(v2);
 			}
 		});
+
+		final ListView listview = (ListView) findViewById(R.id.listView1);
+		final ArrayList<String> list = new ArrayList<String>();
+
+		Cursor phones = getContentResolver().query(
+				ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null,
+				null, null);
+		while (phones.moveToNext()) {
+			String name = phones
+					.getString(phones
+							.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+			String phoneNumber = phones
+					.getString(phones
+							.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+			list.add(name + " " + phoneNumber);
+		}
+		phones.close();
+
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+
+		listview.setAdapter(arrayAdapter);
+
 	}
 
 	@Override
@@ -29,12 +59,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 
-		setTitle("Nouveau Titre");
-		//getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
-		//R.layout.title);
+		// setTitle("Nouveau Titre");
+		// getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
+		// R.layout.title);
 		return true;
 	}
-	
+
 	public void afficherAuthors(View v) {
 		new AlertDialog.Builder(this).setTitle("Auteurs")
 				.setMessage("S. Pruneau & H. Burlini").show();
