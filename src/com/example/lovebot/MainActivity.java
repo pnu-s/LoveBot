be3f8b1a01d7afbd3bcec1758018d000318dd920
SimonPruneau <kartsim.pruneau@gmail.com>
2013-12-15 19:30:37 +0100

    Rajout de la liste des contacts dans Activity à part

--- src/com/example/lovebot/MainActivity.java
+++ src/com/example/lovebot/MainActivity.java
@@ -1,56 +1,40 @@
 package com.example.lovebot;
 
-import java.util.ArrayList;
-
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.database.Cursor;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_LEFT_ICON);
+		//requestWindowFeature(Window.FEATURE_LEFT_ICON);
 		setContentView(R.layout.activity_main);
-		getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
+		/*getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
 				R.drawable.ic_launcher);
+
 		View v = findViewById(android.R.id.title);
 		v.setOnClickListener(new OnClickListener() {
 			public void onClick(View v2) {
 				afficherAuthors(v2);
 			}
+		});*/
+
+		final Button contactsButton = (Button) findViewById(R.id.button1);
+		contactsButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this,
+						ContactsActivity.class);
+				startActivity(intent);
+			}
 		});
-
-		final ListView listview = (ListView) findViewById(R.id.listView1);
-		final ArrayList<String> list = new ArrayList<String>();
-
-		Cursor phones = getContentResolver().query(
-				ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null,
-				null, null);
-		while (phones.moveToNext()) {
-			String name = phones
-					.getString(phones
-							.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-			String phoneNumber = phones
-					.getString(phones
-							.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-			list.add(name + " " + phoneNumber);
-		}
-		phones.close();
-
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, list);
-
-		listview.setAdapter(arrayAdapter);
 
 	}
 
@@ -58,10 +42,6 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-
-		// setTitle("Nouveau Titre");
-		// getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
-		// R.layout.title);
 		return true;
 	}
 
