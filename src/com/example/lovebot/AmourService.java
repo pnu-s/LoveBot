d4571f60ceff4d896ca1c5a5d8092149b03224eb
SimonPruneau <kartsim.pruneau@gmail.com>
2014-01-05 18:01:49 +0100

    Ajout du service permettant de choisir une "cible". A tester sur un vrai téléphone et non émulateur (pour numero de tel). Reste problème récupération et affichage token

---
+++ src/com/example/lovebot/AmourService.java
@@ -0,0 +1,62 @@
+package com.example.lovebot;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.os.AsyncTask;
+
+public class AmourService extends AsyncTask<String, Void, String>
+{
+    @Override
+    protected String doInBackground(String... params)
+    {
+    	//on recup les param
+        String user1 = params[0];
+        String user2 = params[1];
+        
+        //uri en clair
+        String uri = "http://lovebot.byethost17.com/amour.php";
+        
+        // Query string
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("user1", user1));
+        nameValuePairs.add(new BasicNameValuePair("user2", user2));
+        
+        // important : ajout des paramètres dans l'url
+        uri += "?" + URLEncodedUtils.format(nameValuePairs, "utf-8");
+        
+        HttpGet httpGet = new HttpGet(uri);
+        DefaultHttpClient defaultHttpClient = new DefaultHttpClient();
+        try
+        {
+            HttpResponse httpResponse = defaultHttpClient.execute(httpGet, new BasicHttpContext());
+            String response = EntityUtils.toString(httpResponse.getEntity());
+            //on recupere la réponse en JSON
+            JSONObject jsonObject = new JSONObject(response);
+            return jsonObject.getString("token");
+        }
+        catch(JSONException jsonException)
+        {
+        }
+        catch(ClientProtocolException clientProtocolException)
+        {
+
+        }
+        catch(IOException ioException)
+        {
+
+        }
+        return null;
+    }
+}
--- src/com/example/lovebot/ContactsActivity.java
+++ src/com/example/lovebot/ContactsActivity.java
@@ -1,12 +1,19 @@
 package com.example.lovebot;
 
 import java.util.ArrayList;
-
+import java.util.concurrent.ExecutionException;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.database.Cursor;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,8 +23,13 @@ public class ContactsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contacts);
-		
-		final ListView listview = (ListView) findViewById(R.id.listView1);
+
+		TelephonyManager mTelephonyMgr;  
+		mTelephonyMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		final String yourNumber = mTelephonyMgr.getLine1Number();  
+		//new AlertDialog.Builder(this).setTitle("Amour OK").setMessage("Reussi! " + yourNumber).show();
+
+		final ListView lv = (ListView) findViewById(R.id.listView1);
 		final ArrayList<String> list = new ArrayList<String>();
 
 		Cursor phones = getContentResolver().query(
@@ -30,14 +42,55 @@ public class ContactsActivity extends Activity {
 			String phoneNumber = phones
 					.getString(phones
 							.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-			list.add(name + " " + phoneNumber);
+			list.add(name + " | " + phoneNumber);
 		}
 		phones.close();
 
 		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, list);
 
-		listview.setAdapter(arrayAdapter);
+		lv.setAdapter(arrayAdapter);
+		lv.setTextFilterEnabled(true);
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> arg0, View v, int position,
+					long id) {
+				/*
+				 * AlertDialog.Builder adb = new AlertDialog.Builder(lv
+				 * .getContext()); adb.setTitle("ListView OnClick");
+				 * adb.setMessage("Selected Item is = " +
+				 * lv.getItemAtPosition(position) + " and "+ id);
+				 * adb.setPositiveButton("Ok", null); adb.show();
+				 */
+				String user1 = yourNumber;
+				String[] user2 = lv.getItemAtPosition(position).toString().split("\\|");
+
+				// on appelle inscriptionService
+				AmourService amourService = new AmourService();
+				try {
+					// on recupere le token synonyme de liaison réussie
+					String token = amourService.execute(user1, user2[1]).get();
+					if (token != null) {
+						new AlertDialog.Builder(v.getContext())
+								.setTitle("Amour OK")
+								.setMessage("Reussi! " + token).show();
+					} else {
+						new AlertDialog.Builder(v.getContext())
+								.setTitle("Erreur")
+								.setMessage("Erreur :" + token + ": fz "+user2[1]).show();
+					}
+				} catch (InterruptedException interruptedException) {
+					Log.e("log_tag",
+							"interrupted " + interruptedException.toString());
+				} catch (ExecutionException executionException) {
+					Log.e("log_tag",
+							"execution" + executionException.toString());
+				} catch (NullPointerException nullPointerException) {
+					Log.e("log_tag",
+							"nullpointer " + nullPointerException.toString());
+				}
+
+			}
+		});
 	}
 
 	@Override
